93c0a48334aa9e9ca259db06e00acc6152af6a0c
bercerq <nice123414700741@gmail.com>
2021-10-04 15:39:53 +0300

    update main

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-react-helmet`,
     'gatsby-plugin-netlify-cms'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
